ef8a4c2aa969069bbc8f303477b6c34751693457
pinganan <andy406031211@gmail.com>
2023-02-13 12:50:00 +0800

    add tensorboard

--- Meview.py
+++ Meview.py
@@ -33,16 +33,40 @@ def get_file_paths(root, file_type="/"):
 
 
 class Meview(torch.utils.data.Dataset):
-
-    def __init__(self, cfg) -> None:
-        super().__init__()
+    def __init__(self, cfg):
+        self.cfg = cfg
         self.peroid = 15
-        self.thresh = 0.9
-        self.startList = [sum(NUM_FRAMES[:idx])
-                          for idx in range(len(NUM_FRAMES))]
-        self.endList = [sum(NUM_FRAMES[:idx+1])
-                        for idx in range(len(NUM_FRAMES))]
-        self.load_data(cfg)
+        self.sliding = 2
+        self.thresh = 0.8
+
+    def load_validation_data(self, assignID):
+        subject = SUBJECTS[assignID]
+        input_paths = get_file_paths(
+            f'{self.cfg.PATH_TO_DATASET}/{subject}', '.png')
+        images = torch.stack([read_image(path) for path in input_paths])
+        labels = torch.Tensor(
+            [1 if ONSET[assignID] <= i < OFFSET[assignID] else 0 for i in range(len(images))])
+        self.train_data = images.unfold(
+            0, self.peroid, self.sliding).type(torch.float32) / 255.0
+        self.train_data = self.train_data.permute(0, 4, 1, 2, 3)
+        self.train_label = labels.unfold(
+            0, self.peroid, self.sliding).type(torch.long)
+
+    def under_sampling(self, sid, images, labels):
+        last_images, last_labels = [], []
+        rmList = torch.rand(NUM_FRAMES[sid]-self.peroid)
+        for idx, value in enumerate(rmList):
+            # including positive in peroid
+            if idx < OFFSET[sid] and idx+self.peroid > ONSET[sid]:
+                pass
+            # random selected
+            elif value > self.thresh:
+                pass
+            else:
+                continue
+            last_images.append(images[idx:idx+self.peroid])
+            last_labels.append(labels[idx:idx+self.peroid])
+        return torch.stack(last_images), torch.stack(last_labels)
 
     # total data including train/val
     def load_data(self, cfg):
@@ -59,23 +83,48 @@ class Meview(torch.utils.data.Dataset):
         self.train_data = train_data.type(torch.float32) / 255.0
         self.train_label = train_label.type(torch.long)
 
-    def searchList_to_trainList(self, exceptID=-1):
-        self.itemList = list()
-        for sid in range(len(self.startList)):
+    def __len__(self):
+        return len(self.train_data)
+
+    def __getitem__(self, index):
+        return self.train_data[index], self.train_label[index], torch.Tensor([1 for _ in range(self.peroid)])
+
+
+class CheatedMeview(Meview):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    def load_validation_data(self, assignID):
+        subject = SUBJECTS[assignID]
+        input_paths = get_file_paths(
+            f'{self.cfg.PATH_TO_DATASET}/{subject}', '.png')
+        images = torch.stack([self.toCheat2(read_image(path)) if ONSET[assignID] <= i < OFFSET[assignID] else self.toCheat(
+            read_image(path)) for i, path in enumerate(input_paths)])
+        labels = torch.Tensor(
+            [1 if ONSET[assignID] <= i < OFFSET[assignID] else 0 for i in range(len(images))])
+        self.train_data = images.unfold(
+            0, self.peroid, self.sliding).type(torch.float32) / 255.0
+        self.train_data = self.train_data.permute(0, 4, 1, 2, 3)
+        self.train_label = labels.unfold(
+            0, self.peroid, self.sliding).type(torch.long)
+
+    # format-> batch_size, num_steps, c, h, w = x.shape
+    def load_traning_data(self, exceptID=-1):
+        train_data, train_label = torch.Tensor(), torch.Tensor()
+        for sid, subject in enumerate(SUBJECTS):
             if sid == exceptID:
                 continue
-            start, end = self.startList[sid], self.endList[sid]
-            for id in range(start, end-self.peroid):
-                # in positive
-                if start+ONSET[sid] < id+self.peroid and id < start+OFFSET[sid]:
-                    self.itemList.append(id)
-                # random negative
-                elif random.random() > self.thresh:
-                    self.itemList.append(id)
-
-    def searchList_to_valList(self, selectedID=-1):
-        start, end = self.startList[selectedID], self.endList[selectedID]
-        self.itemList = list(range(start, end-self.peroid))
+            input_paths = get_file_paths(
+                f'{self.cfg.PATH_TO_DATASET}/{subject}', '.png')
+            images = torch.stack([self.toCheat2(read_image(path)) if ONSET[sid] <= i < OFFSET[sid] else self.toCheat(
+                read_image(path)) for i, path in enumerate(input_paths)])
+            labels = torch.Tensor(
+                [1 if ONSET[sid] <= i < OFFSET[sid] else 0 for i in range(len(images))])
+            images, labels = self.under_sampling(sid, images, labels)
+            train_data = torch.cat((train_data, images))
+            train_label = torch.cat((train_label, labels))
+        self.train_data = train_data.type(torch.float32) / 255.0
+        self.train_label = train_label.type(torch.long)
 
     def __len__(self):
         return len(self.itemList)
@@ -92,10 +141,10 @@ if __name__ == '__main__':
 
     args = parse_args()
     cfg = load_config(args)
-    dataset = Meview(cfg)
+    dataset = CheatedMeview(cfg)
+    dataset.load_traning_data(1)
     print(dataset.train_data.shape)
-    dataset.searchList_to_valList()
-    data, label, mask = dataset.__getitem__(20)
-    print(data.shape)
-    print(label.shape)
-    print(mask.shape)
+    print(dataset.train_label.shape)
+    dataset.load_cheatedTraning_data(1)
+    print(dataset.train_data.shape)
+    print(dataset.train_label.shape)
--- train.py
+++ train.py
@@ -6,10 +6,11 @@ from datetime import datetime
 from utils.parser import parse_args, load_config
 from models import build_model, load_checkpoint
 from utils.optimizer import construct_optimizer, construct_scheduler
-from Meview import Meview, CheatMeview
+from Meview import Meview, CheatedMeview
 from torch.utils.data import DataLoader
 from algos import get_algo
 from sklearn.metrics import f1_score, recall_score, confusion_matrix
+from torch.utils.tensorboard import SummaryWriter
 
 
 SUBJECTS = ["sub01_01", "sub02_01", "sub03_01", "sub05_02", "sub06_01",
@@ -24,84 +25,81 @@ def classify_evaluation(gt, pred):
     return f1, recall
 
 
-def train(cfg, train_loader, model, optimizer, scheduler, algo, cur_epoch, writer):
+def train(cfg, train_loader, model, optimizer, algo, cur_epoch, tf_writer, txt_writer):
     global trainSubjectID
-    model.train()
-    optimizer.zero_grad()
-    train_loader = tqdm(train_loader, total=len(train_loader),
-                        desc=f'training {SUBJECTS[trainSubjectID]}')
-
-    loss_record = []
+    tmp_subject = SUBJECTS[trainSubjectID]
     groundTruth, predicts = [], []
-    for videos, labels, video_masks in train_loader:
+
+    model.train()
+    train_loader = tqdm(train_loader, total=len(train_loader),
+                        desc=f'training {tmp_subject}')
+    for batch_step, (videos, labels, video_masks) in enumerate(train_loader):
         optimizer.zero_grad()
         videos = videos.cuda()
         labels = labels.cuda()
         video_masks = video_masks.cuda()
-        loss_dict, gts, preds = algo.compute_loss(
-            model, videos, labels, video_masks)
+        loss_dict, gts, preds = algo.compute_loss(model, videos, labels, video_masks)
         loss = loss_dict["loss"]
         loss.backward()
-        loss_record.append(loss.item())
-        groundTruth = torch.cat((groundTruth, gts))
-        predicts = torch.cat((predicts, preds))
 
         # Update the parameters.
         torch.nn.utils.clip_grad_norm_(
             model.parameters(), cfg.OPTIMIZER.GRAD_CLIP)
         optimizer.step()
-
         for key in loss_dict:
             loss_dict[key][torch.isnan(loss_dict[key])] = 0
 
-    loss_record = [round(float(temp), 3) for temp in loss_record]
-    writer.write(
-        f"[loss] {str(loss_record)[1:-1]}\n")
-    groundTruth = groundTruth.cpu()
-    predicts = predicts.cpu()
+        # Record Loss into tensorboard
+        tf_writer.add_scalar(f'{tmp_subject} / Train - loss',
+                             loss.item(), cur_epoch*len(train_loader)+batch_step)
+
+        groundTruth += gts.cpu()
+        predicts += preds.cpu()
+    # Record each subject f1, recall, confusion matrix
     f1, recall = classify_evaluation(groundTruth, predicts)
     tn, fp, fn, tp = confusion_matrix(
         groundTruth, predicts, labels=[0, 1]).ravel()
-    writer.write(
-        f"[train result] {tp=:5d}, {tn=:5d}, {fp=:5d}, {fn=:5d}, {f1=:.3f}, {recall=:.3f}\n")
-
-    if cur_epoch != cfg.TRAIN.MAX_EPOCHS-1:
-        scheduler.step()
+    txt_writer.write(
+        f"\tTrain result\t{tp=:5d}, {tn=:5d}, {fp=:5d}, {fn=:5d}, {f1=:.3f}, {recall=:.3f}\n")
+    print(f"train Pos/Neg = {sum(groundTruth)} / {len(groundTruth)-sum(groundTruth)}")
 
 
-def val(val_loader, model, algo, writer):
-    model.eval()
-
+def val(val_loader, model, algo, cur_epoch, tf_writer, txt_writer):
+    global trainSubjectID
+    tmp_subject = SUBJECTS[trainSubjectID]
     groundTruth, predicts = [], []
+
+    model.eval()
     with torch.no_grad():
-        for videos, labels, video_masks in val_loader:
+        for batch_step, (videos, labels, video_masks) in enumerate(val_loader):
             videos = videos.cuda()
             labels = labels.cuda()
             video_masks = video_masks.cuda()
             loss_dict, gts, preds = algo.compute_loss(
                 model, videos, labels, video_masks, training=False)
-            groundTruth = torch.cat((groundTruth, gts))
-            predicts = torch.cat((predicts, preds))
-            for key in loss_dict:
-                loss_dict[key][torch.isnan(loss_dict[key])] = 0
+            loss = loss_dict["loss"]
 
-    groundTruth = groundTruth.cpu()
-    predicts = predicts.cpu()
-    f1, recall = classify_evaluation(groundTruth, predicts)
-    tn, fp, fn, tp = confusion_matrix(
-        groundTruth, predicts, labels=[0, 1]).ravel()
-    writer.write(
-        f"[valid result] {tp=:5d}, {tn=:5d}, {fp=:5d}, {fn=:5d}, {f1=:.3f}, {recall=:.3f}\n")
+            # Record Loss into tensorboard
+            tf_writer.add_scalar(f'{tmp_subject} / Valid - loss',
+                                 loss.item(), cur_epoch*len(val_loader)+batch_step)
+
+            groundTruth += gts.cpu()
+            predicts += preds.cpu()
+        # Record each subject f1, recall, confusion matrix
+        f1, recall = classify_evaluation(groundTruth, predicts)
+        tn, fp, fn, tp = confusion_matrix(
+            groundTruth, predicts, labels=[0, 1]).ravel()
+        txt_writer.write(
+            f"\tValid result\t{tp=:5d}, {tn=:5d}, {fp=:5d}, {fn=:5d}, {f1=:.3f}, {recall=:.3f}\n")
+        print(f"valid Pos/Neg = {sum(groundTruth)} / {len(groundTruth)-sum(groundTruth)}")
 
 
-def main(trainSubjectID, writer):
+def main(trainSubjectID, tf_writer, txt_writer):
     args = parse_args()
     cfg = load_config(args)
-
     algo = get_algo(cfg)
     model = build_model(cfg)
     optimizer = construct_optimizer(model, cfg)
-    scheduler = construct_scheduler(optimizer, cfg)
     load_checkpoint(cfg, model, optimizer)
     # for name, param in model.named_parameters():
     #     if "classifier" in name:
@@ -112,22 +110,31 @@ def main(trainSubjectID, writer):
     #         param.requires_grad = False
     model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
-    train_dataset = CheatMeview(cfg)
+    train_dataset = Meview(cfg)
     train_dataset.load_traning_data(trainSubjectID)
-    train_loader = DataLoader(train_dataset, batch_size=cfg.TRAIN.BATCH_SIZE, shuffle=True)
-    val_dataset = CheatMeview(cfg)
-    val_dataset.load_specific_data(trainSubjectID)
+    train_loader = DataLoader(train_dataset, batch_size=cfg.TRAIN.BATCH_SIZE,
+                              shuffle=True)
+    val_dataset = Meview(cfg)
+    val_dataset.load_validation_data(trainSubjectID)
     val_loader = DataLoader(val_dataset, batch_size=cfg.TRAIN.BATCH_SIZE)
 
     """Trains model and evaluates on relevant downstream tasks."""
-    for cur_epoch in range(3):
-        train(cfg, train_loader, model, optimizer, scheduler, algo, cur_epoch, writer)
-    val(val_loader, model, algo, writer)
+    print(f"Start to Train {SUBJECTS[trainSubjectID]}")
+    txt_writer.write(f"{SUBJECTS[trainSubjectID]}\n")
+    for cur_epoch in range(5):
+        train(cfg, train_loader, model, optimizer, algo, cur_epoch, tf_writer, txt_writer)
+    val(val_loader, model, algo, cur_epoch, tf_writer, txt_writer)
+    txt_writer.write(f"\n")
+    print(f"Finish {SUBJECTS[trainSubjectID]}")
 
 
 if __name__ == '__main__':
-    for trainSubjectID in range(19):
-        now = datetime.now()
-        sub_path = f"./train_logs/cheat_{now.year}_{now.month:02d}_{now.day:02d}_{now.hour:02d}_{SUBJECTS[trainSubjectID]}.txt"
-        with open(sub_path, "w") as writer:
-            main(trainSubjectID, writer)
+    now = datetime.now()
+    path = f"{now.month:02d}_{now.day:02d}_{now.hour:02d}"
+    tf_writer = SummaryWriter(log_dir=f"runs/{path}/")
+    for trainSubjectID in range(len(SUBJECTS)):
+        txt_writer = open(f"./train_logs/{path}_{SUBJECTS[trainSubjectID]}.txt", "w")
+        main(trainSubjectID, tf_writer, txt_writer)
+        txt_writer.close()
+    tf_writer.close()
+    
